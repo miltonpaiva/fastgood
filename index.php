0d38255fade832796d5de892997d974ca79b9958
miltonpaiva <miltonpaiva268@gmail.com>
2019-11-09 16:01:51 -0200

    refactor: adição de comentarios - index

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
 <?php
-
+	/**
+	 * [$project_path Contem o path padrão da aplicação]
+	 * @var [string]
+	 */
 	$project_path = current(explode('index.php', $_SERVER['SCRIPT_NAME']));
+
+	/**
+	 * [$project_name Nome da aplicação]
+	 * @var string
+	 */
 	$project_name = 'Fast GOOD';
 
 	require_once $_SERVER['DOCUMENT_ROOT'] . "{$project_path}includes/connect_db.php";
 	require_once $_SERVER['DOCUMENT_ROOT'] . "{$project_path}app/controller.php";
 
+	/**
+	 * [$pdo instancia da conexão com o banco]
+	 * @var [Object]
+	 */
 	$pdo = Database::conexao();
 
+	/**
+	 * [$inc Instancia da Classe Controller]
+	 * @var Controller
+	 */
 	$inc = new Controller();
 
+	/**
+	 * @param  [String]
+	 * @return [Array]
+	 */
 	function formatPath($path_info)
 	{
 		$data = explode('/', $path_info);
@@ -21,6 +41,10 @@
 		return $data;
 	}
 
+	/**
+	 * [$path_info define as ações na aplicação]
+	 * @var [Array]
+	 */
 	$path_info = formatPath(@$_SERVER['PATH_INFO']);
 
 	if (count($path_info) == 1){
